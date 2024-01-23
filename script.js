541cc524f85661d133b585660f5897b098f1e6e4
Cheshan782 <88943662+Cheshan782@users.noreply.github.com>
2024-01-23 11:08:46 -0500

    Sum

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ var currentLeft = Math.floor(Math.random() * documentWidth) + 1;
 blueMassElement.style.top = currentTop + "px";
 blueMassElement.style.left = currentLeft + "px";
 
+const url = 'https://famous-quotes4.p.rapidapi.com/random?category=all&count=2';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1e8b1e0089mshc1cdd42625920fcp1664b8jsn0e118e27aa4c',
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
