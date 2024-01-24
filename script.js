64da7770a4046d04bc6ac9c247efb33f7de626d5
Cheshan782 <88943662+Cheshan782@users.noreply.github.com>
2024-01-23 19:44:53 -0500

    Final

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-var blueMassElement = document.getElementById("bluemass");
-var documentHeight = document.documentElement.clientHeight;
-var documentWidth = document.documentElement.clientWidth;
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
 
-// Create the divs in here
+const url = "https://api.quotable.io/random";
 
-var currentTop = Math.floor(Math.random() * documentHeight) + 1;
-var currentLeft = Math.floor(Math.random() * documentWidth) + 1;
-
-blueMassElement.style.top = currentTop + "px";
-blueMassElement.style.left = currentLeft + "px";
-
-const url = 'https://famous-quotes4.p.rapidapi.com/random?category=all&count=2';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1e8b1e0089mshc1cdd42625920fcp1664b8jsn0e118e27aa4c',
-		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
-	}
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      document.getElementById("Quote").textContent = item.content;
+      console.log(item.content);
+      console.log(item.author);
+      quote.innerText = item.content;
+      author.innerText = item.author;
+    });
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+window.addEventListener("load", getQuote);
